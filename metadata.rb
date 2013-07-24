f4953a36c18984089acc98afed65d3a3b5a69124
Gabriel Monroy <gabriel@opdemand.com>
2013-07-24 09:45:13 -0600

    downgrade version to 0.0.4 to match main project

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'deis'
 maintainer       'OpDemand'
 maintainer_email 'gabriel@opdemand.com'
-license          'All rights reserved'
-description      'Installs/Configures deis'
+license          'Apache 2.0'
+description      'Installs/Configures Deis PaaS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.4'
 
 depends          'apt'
 depends          'sudo'
