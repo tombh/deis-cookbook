5e3da772053be203a1460218d9737628356443a3
Gabriel Monroy <gabriel@opdemand.com>
2013-09-09 15:43:00 -0600

    set branch to application-refactor

--- attributes/default.rb
+++ attributes/default.rb
@@ -32,7 +32,7 @@ default.deis.database.user = 'deis'
 # server/api
 default.deis.controller.dir = '/opt/deis/controller'
 default.deis.controller.repository = 'https://github.com/opdemand/deis.git'
-default.deis.controller.revision = 'master'
+default.deis.controller.revision = 'application-refactor'
 default.deis.controller.debug = 'False'
 default.deis.controller.workers = 4
 default.deis.controller.worker_port = 8000
