d209bd0e9bffc992bbe16af944389c955d5f9726
Paul Czarkowski <paul.czarkowski@rackspace.com>
2014-01-21 11:12:04 -0600

    skip docker steps if already installed
    This will reduce lookups to get.docker.io on chef runs
    * save a small amount of time for chef runs
    * if get.docker.io is unavailable it won't break run.

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -6,9 +6,12 @@ apt_repository 'dotcloud' do
   uri node['deis']['docker']['deb_url']
   distribution 'docker'
   components ['main']
+  not_if { File.exists?('/etc/apt/sources.list.d/dotcloud.list') }
 end
 
-package "lxc-docker-#{node['deis']['docker']['version']}"
+package "lxc-docker-#{node['deis']['docker']['version']}" do
+  not_if { "/usr/bin/dpkg -s lxc-docker-#{node['deis']['docker']['version']}" }
+end
 
 service 'docker' do
   provider Chef::Provider::Service::Upstart
