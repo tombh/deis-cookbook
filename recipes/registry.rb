9dbdf2bdcf11dbed2cdf0440c8ff31d70d250c5b
Gabriel Monroy <gabriel@opdemand.com>
2014-01-26 17:10:09 -0700

    new deis/registry component and recipe

--- recipes/registry.rb
+++ recipes/registry.rb
@@ -1,52 +1,28 @@
 
-# configure the container unless its keyspace already exists
-
-require 'etcd'
-
-# TODO: refactor into library
-ruby_block 'set-registry-config' do
-  block do
-    client = Etcd.client(host: node.deis.public_ip, port: node.deis.etcd.port)
-    client.set('/deis/registry/backend', 'local')
-  end
-  not_if {
-    client = Etcd.client(host: node.deis.public_ip, port: node.deis.etcd.port)
-    begin
-      client.get('/deis/registry')
-      true
-    rescue
-      false
-    end
-  }
+docker_image node.deis.registry.image do
+  source node.deis.registry.source
+  only_if "test -e #{node.deis.registry.source}/Dockerfile"
+  action :build
 end
 
-# TODO: refactor into library
-env = []
-ruby_block 'get-registry-config' do
-  block do
-    client = Etcd.client(host: node.deis.public_ip, port: node.deis.etcd.port)
-    env.push("REGISTRY_BACKEND="+client.get('/deis/registry/backend').value)
-  end
+docker_container node.deis.registry.container do
+  container_name node.deis.registry.container
+  detach true
+  privileged true
+  env ["ETCD=#{node.deis.public_ip}:#{node.deis.etcd.port}",
+       "HOST=#{node.deis.public_ip}",
+       "PORT=#{node.deis.registry.port}"]
+  image node.deis.registry.image
+  init_type false  
+  port "#{node.deis.registry.port}:#{node.deis.registry.port}"
+  # bind mount /app if we're running out of vagrant
+  volume File.exist?('/vagrant/images/registry') ? "/vagrant/images/registry:/app" : nil
+  cmd_timeout 600 # image takes a while to download
 end
 
-# TODO: refactor into library
-ruby_block 'publish-registry' do
+ruby_block 'wait-for-registry' do
   block do
-    client = Etcd.client(host: node.deis.public_ip, port: node.deis.etcd.port)
-    client.set('/deis/registry/host', node.deis.public_ip)
-    client.set('/deis/registry/port', node.deis.registry.port)
+    EtcdHelper.wait_for_key(node.deis.public_ip, node.deis.etcd.port,
+                            '/deis/registry/host')
   end
-  #action :nothing
 end
-
-# start the container and create an upstart service
-
-# docker_container node.deis.registry.container do
-  # container_name node.deis.registry.container
-  # detach true
-  # env env
-  # image node.deis.registry.image
-  # port "#{node.deis.registry.port}:#{node.deis.registry.port}"
-  # notifies :create, "ruby_block[publish-registry]", :immediately
-# end
-
