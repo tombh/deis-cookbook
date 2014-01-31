b43ef8e5af897b9da0f4dd2256509b98e8fd0f2d
Gabriel Monroy <gabriel@opdemand.com>
2014-01-31 13:39:25 -0700

    first pass at docker image based runtime.rb

--- recipes/runtime.rb
+++ recipes/runtime.rb
@@ -1,40 +1,5 @@
 include_recipe 'rsyslog::client'
 
-# directory node.deis.runtime.dir do
-  # user node.deis.username
-  # group node.deis.group
-  # mode 0700
-# end
-# 
-# directory node.deis.runtime.slug_dir do
-  # user node.deis.username
-  # group node.deis.group
-  # mode 0700
-# end
-# 
-# git node.deis.runtime.runner_dir do
-  # user node.deis.username
-  # group node.deis.group
-  # repository node.deis.runtime.repository
-  # revision node.deis.runtime.revision
-  # action :sync
-# end
-# 
-# bash 'create-slugrunner-image' do
-  # cwd node.deis.runtime.runner_dir
-  # code 'docker build -t deis/slugrunner .'
-  # not_if 'docker images | grep deis/slugrunner'
-# end
-
-# TODO: add back when https://github.com/dotcloud/docker/issues/643 is fixed 
-#bash 'clear-docker-containers' do
-#  code 'docker rm `docker ps -a -q`'
-#  action :nothing
-#  subscribes :run, 'bash[create-slugrunner-image]', :immediately
-#end
-
-package 'curl'
-
 formations = data_bag('deis-formations')
 
 services = []
@@ -57,38 +22,9 @@ formations.each do |f|
     version = app['release']['version']
     build = app['release']['build']
     config = app['release']['config']
-    
-    # if build is specified, use special heroku-style runtime
-    
-    if build.has_key? 'url'
-    
-      slug_url = build['url']
-      
-      # download the slug to a tempdir
-      slug_root = node.deis.runtime.slug_dir
-      slug_path = "#{slug_root}/#{app_id}-v#{version}.tar.gz"
-      slug_dir = "#{slug_root}/#{app_id}-v#{version}"
-      
-      bash "download-slug-#{app_id}-#{version}" do
-        cwd slug_root
-        code <<-EOF
-          rm -rf #{slug_dir}
-          mkdir -p #{slug_dir}
-          cd #{slug_dir}
-          curl -s #{slug_url} > #{slug_path}
-          tar xfz #{slug_path}
-          rm #{slug_path}
-          EOF
-        not_if "test -d #{slug_dir}"
-      end
-
-      # will prevent deleted these in the SLUG_DIR step
-      active_slug_paths.push("#{app_id}-v#{version}")
-
-    end
+    image = build['image']
   
-    # iterate over this application's process formation by
-    # Procfile-defined type
+    # iterate over this application's containers by process type
     
     app['containers'].each_pair do |c_type, c_formation|
       
@@ -99,10 +35,10 @@ formations.each do |f|
         next if nodename != node.name
 
         # determine build command, if one exists
-        if build != {}
-          command = build['procfile'][c_type]
+        if build['procfile'] != nil and build['procfile'].has_key? c_type
+          command = "start #{c_type}"
         else
-          command = nil # assume command baked into docker image
+          command = nil
         end
         name = "#{app_id}.#{c_type}.#{c_num}"
         # define the container
@@ -112,33 +48,14 @@ formations.each do |f|
           c_num c_num
           env config
           port port
-          image 'deis/slugrunner'
-          slug_dir slug_dir
+          image image
+          command command
         end
         services.push("deis-#{name}")
       end
     end
   end # formations['apps'].each
 end # formations.each
-
-
-# remove old slug dirs
-slug_root = node.deis.runtime.slug_dir
-if Dir.exists?(slug_root)
-  Dir.entries(slug_root).each do |f|
-    next if f == '.'
-    next if f == '..'
-
-    slug_dir = "#{slug_root}/#{f}"
-
-    directory slug_dir do
-      action :delete
-      recursive true
-      not_if { active_slug_paths.include? f }
-    end
-  end
-end
-
 
 # 
 # # purge old container services
