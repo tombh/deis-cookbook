7eaa8087d77833c7b90dacebf9c0cfc54b7f1c26
Gabriel Monroy <gabriel@opdemand.com>
2014-01-31 22:24:21 -0700

    explain server bind mounts

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,8 +8,14 @@ docker_container node.deis.server.container do
   image node.deis.server.image
   init_type false
   port "#{node.deis.server.port}:#{node.deis.server.port}"
-  # bind mount /app if we're running out of vagrant
-  volume [ "/home/vagrant:/home/vagrant", File.exist?('/vagrant/manage.py') ? "/vagrant:/app/deis" : nil,
+  volume [
+          # share log directory between server and logger components
+          # TODO: replace with a distributed mechanism for populating `deis logs`
+          "#{node.deis.log_dir}:/app/deis/logs",
+          # required by vagrant provider machinery
+          '/home/vagrant:/home/vagrant',
+          # bind mount /app if we're running out of vagrant
+           File.exist?('/vagrant/manage.py') ? "/vagrant:/app/deis" : nil,
            File.exist?('/vagrant/images/server') ? "/vagrant/images/server/bin:/app/bin" : nil,
            File.exist?('/vagrant/images/server') ? "/vagrant/images/server/conf.d:/app/conf.d" : nil,
            File.exist?('/vagrant/images/server') ? "/vagrant/images/server/templates:/app/templates" : nil,]
