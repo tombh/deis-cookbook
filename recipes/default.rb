1a6e00d454959462fae14803d91d4ab5c80b6a94
Gabriel Monroy <gabriel@opdemand.com>
2014-02-10 14:59:52 -0700

    hardcode docker version to 0.8.0 re opdemand/deis#516

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,58 +2,50 @@
 # Cookbook Name:: deis
 # Recipe:: default
 #
-# Copyright 2013, OpDemand LLC
+# Copyright 2014, OpDemand LLC
 #
 
 # install etcd bindings
-
 chef_gem 'etcd'
 
 # bind docker to all interfaces for external connectivity
 node.default['docker']['bind_uri'] = 'tcp://0.0.0.0:4243'
-
+# hardcode specific docker version
+node.default['docker']['version'] = '0.8.0'
+# install docker through chef-docker
 include_recipe 'docker'
 
-# always install these packages
-
+# install required packages
 package 'fail2ban'
 package 'git'
 package 'make'
 
-# set public ip
-
+# set public ip via Ohai if not defined
 if node.deis.public_ip == nil
-    log "ip-discovery-warning" do
-      message "\nPublic IP attribute not provided, using Ohai: #{node.ipaddress}"
-      level :warn
-    end
-    node.default.deis.public_ip = node.ipaddress
+  node.default.deis.public_ip = node.ipaddress
 end
-
-home_dir = node.deis.dir
-username = node.deis.username
 
 # create deis user with ssh access, auth keys
 # and the ability to run 'sudo chef-client'
 
-user username do
+user node.deis.username do
   system true
   uid 324 # "reserved" for deis
   shell '/bin/bash'
   comment 'deis system account'
-  home home_dir
+  home node.deis.dir
   supports :manage_home => true
   action :create
 end
 
-directory home_dir do
-  user username
-  group username
+directory node.deis.dir do
+  user node.deis.username
+  group node.deis.username
   mode 0755
 end
 
-sudo username do
-  user  username
+sudo node.deis.username do
+  user node.deis.username
   nopasswd  true
   commands ['/usr/bin/chef-client']
 end
