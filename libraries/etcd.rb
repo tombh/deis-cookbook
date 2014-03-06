dfdc60c6ca568e727cc57b0c8525ec7cef8c5762
Tom Buckley-Houston <tom@tombh.co.uk>
2014-03-06 22:48:57 +0000

    Instead of using a timeout to wait for booting containers, poll thier exit code.

--- libraries/etcd.rb
+++ libraries/etcd.rb
@@ -1,26 +1,30 @@
-
-require 'timeout'
+require 'json'
 
 class Chef::Recipe::EtcdHelper
+
+  # Get the current exit code for a container
+  def self.get_exit_code key
+    # This line is just a hack so I don't have to go changing any other files. If this whole
+    # approach proves successful then we can change the args in wait_for_key to pass the
+    # actual container name. Also it won't need to pass timeouts.
+    key = key.gsub('logs', 'logger').gsub('controller', 'server')
+
+    container = key.split('/')[1..-2].join('-')
+    JSON.parse(`sudo docker inspect #{container}`)[0]['State']['ExitCode']
+  end
 
   def self.wait_for_key(host, port, k, seconds=30)
     # inline import to avoid load-time gem requirement
     require 'etcd'
-    client = Etcd.client(host: host, port: port)    
-    begin
-      Timeout::timeout(seconds) do
-        while true do
-          begin       
-            client.get(k)
-            break
-          rescue => e
-            sleep 1
-            next
-          end
-        end
+    client = Etcd.client(host: host, port: port)
+    while true do
+      raise Chef::Exceptions::Service if get_exit_code(k) != 0
+      begin
+        client.get(k)
+        break # Because container successfully booted
+      rescue => e
+        sleep 1
       end
-    rescue Timeout::Error
-      raise
     end
   end
 
